3f97be7c1c9bc7b999ebe007e0ef9523978d8324
Stephen Gordon <sgordon@paloaltonetworks.com>
2024-03-19 10:14:09 -0500

    add try catch blocks and file reorganization

--- collections-from-deployed/poc.py
+++ collections-from-deployed/poc.py
@@ -2,14 +2,15 @@
 import sys
 import os
 
-
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
-sys.path.append(parent_dir)
+sys.path.append(os.path.expanduser("~/repos/pcs-platform-helper-scripts/poc"))
 
-from utilities.main import *
+print(parent_dir)
 
-config_file = os.path.join(os.path.expanduser("~/.prismacloud/poc.json"))
+from poc.utilities.main import *
+
+config_file = os.path.join(os.path.expanduser("~/.prismacloud/credentials.json"))
 
 
 my_pc = connect(config_file)
